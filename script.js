a7a3d4bb3ebf8a19b2300e5f727e92cab24c1ccb
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-04-19 08:26:35 -0500

    agregado formulario

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ console.log('Hello!');
 function cambiaColor() {
     document.getElementById("texto").style.color = "blue";
 }
+
+function validar() {
+    // Obtenemos los valores de los campos
+    var nombres = document.getElementById("nombres").value.trim();
+    var apellido = document.getElementById("apellido").value.trim();
+    var telefono = document.getElementById("telefono").value.trim();
+    var email = document.getElementById("email").value.trim();
+
+    // Validamos los campos
+    if (nombres === "") {
+        document.getElementById("error-nombres").innerHTML = "Por favor, ingrese su nombre";
+        return false;
+    }
+    if (apellido === "") {
+        document.getElementById("error-apellido").innerHTML = "Por favor, ingrese su apellido";
+        return false;
+    }
+    if (telefono === "") {
+        document.getElementById("error-telefono").innerHTML = "Por favor, ingrese su número de teléfono";
+        return false;
+    }
+    if (email === "") {
+        document.getElementById("error-email").innerHTML = "Por favor, ingrese su correo electrónico";
+        return false;
+    }
+    // Si todos los campos son válidos, enviamos el formulario
+    return true;
+}
