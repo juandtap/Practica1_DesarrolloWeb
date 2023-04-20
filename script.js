ec120dc6fc79b368ec38f7be3ea803ccbc69f800
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-04-19 22:53:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,19 +32,19 @@ function validar() {
 
     // Validamos los campos
     if (nombres === "") {
-        document.getElementById("error-nombres").innerHTML = "Por favor, ingrese su nombre";
+        
         return false;
     }
     if (apellido === "") {
-        document.getElementById("error-apellido").innerHTML = "Por favor, ingrese su apellido";
+       
         return false;
     }
     if (telefono === "") {
-        document.getElementById("error-telefono").innerHTML = "Por favor, ingrese su número de teléfono";
+       
         return false;
     }
     if (email === "") {
-        document.getElementById("error-email").innerHTML = "Por favor, ingrese su correo electrónico";
+        
         return false;
     }
     // Si todos los campos son válidos, enviamos el formulario
